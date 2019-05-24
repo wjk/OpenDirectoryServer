fef73dc78151a6454f3ba4b429e073ac09bc4742
William Kent <wjk011@gmail.com>
2019-05-24 18:19:26 -0400

    Add script to uninsstall the helper tool

---
+++ PrivilegedHelperTool/Uninstall.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+sudo launchctl unload /Library/LaunchDaemons/me.sunsol.OpenDirectoryServer.PrivilegedHelperTool.plist
+sudo rm /Library/LaunchDaemons/me.sunsol.OpenDirectoryServer.PrivilegedHelperTool.plist
+sudo rm /Library/PrivilegedHelperTools/me.sunsol.OpenDirectoryServer.PrivilegedHelperTool
+
+sudo security -q authorizationdb remove me.sunsol.OpenDirectoryServer.createMaster
+sudo security -q authorizationdb remove me.sunsol.OpenDirectoryServer.createReplica
+sudo security -q authorizationdb remove me.sunsol.OpenDirectoryServer.destroy
+sudo security -q authorizationdb remove me.sunsol.OpenDirectoryServer.createBackup
+sudo security -q authorizationdb remove me.sunsol.OpenDirectoryServer.restoreBackup
