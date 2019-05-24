b9b5827b948f3bebe51cf11b93c91f4a31310567
William Kent <wjk011@gmail.com>
2019-05-24 18:03:07 -0400

    Add HelperTool class

---
+++ PrivilegedHelperTool/HelperTool.swift
@@ -0,0 +1,46 @@
+//
+//  HelperTool.swift
+//  PrivilegedHelperTool
+//
+//  Created by William Kent on 5/24/19.
+//  Copyright © 2019 William Kent. All rights reserved.
+//
+
+import Foundation
+
+internal final class HelperTool: NSObject, HelperToolRequestProtocol {
+	func getProtocolVersion(reply: @escaping (String) -> Void) {
+		reply(HelperToolProtocolVersion)
+	}
+
+	func createOpenDirectoryMaster(parameters: [String : String], reply: @escaping (NSNumber?, NSError?) -> Void) {
+		// Not yet implemented.
+		let error = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+		reply(nil, error)
+	}
+
+	func createOpenDirectoryReplica(parameterrs: [String : String], reply: @escaping (NSNumber?, NSError?) -> Void) {
+		// Not yet implemented.
+		let error = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+		reply(nil, error)
+	}
+
+	func destroyOpenDirectoryServer(adminUserName: String?, reply: @escaping (NSNumber?, NSError?) -> Void) {
+		// Not yet implemented.
+		let error = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+		reply(nil, error)
+	}
+
+	func createOpenDirectoryBackup(backupLocation: URL, reply: @escaping (NSNumber?, NSError?) -> Void) {
+		// Not yet implemented.
+		let error = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+		reply(nil, error)
+	}
+
+	func restoreOpenDirectoryBackup(backupLocation: URL, reply: @escaping (NSNumber?, NSError?) -> Void) {
+		// Not yet implemented.
+		let error = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+		reply(nil, error)
+	}
+
+}
