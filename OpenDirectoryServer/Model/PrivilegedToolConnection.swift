56e85b2fd6e083df8ce381d8c9735d714a899355
William Kent <wjk011@gmail.com>
2019-06-05 19:48:03 -0400

    Move privileged tool installation code

--- OpenDirectoryServer/Model/PrivilegedToolConnection.swift
+++ OpenDirectoryServer/Model/PrivilegedToolConnection.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import ServiceManagement
 
 internal enum PrivilegedToolConnection {
 	static var isAvailable: Bool {
@@ -47,5 +48,27 @@ internal enum PrivilegedToolConnection {
 		connection.remoteObjectInterface = NSXPCInterface(with: HelperToolRequestProtocol.self)
 
 		return connection
+	}
+
+	static func updateConnection() throws {
+		guard let authRef = try Authorization.emptyAuthorizationRef() else {
+			fatalError("Could not create empty AuthorizationRef")
+		}
+
+		let right = AuthorizationRight(name: kSMRightModifySystemDaemons, description: "")
+		try Authorization.verifyAuthorization(authRef, forAuthorizationRight: right, promptText: nil)
+
+		var error: Unmanaged<CFError>? = nil
+		let success = SMJobBless(kSMDomainSystemLaunchd, "me.sunsol.OpenDirectoryServer.PrivilegedHelperTool" as CFString, authRef, &error)
+		if !success {
+			if let error = error {
+				throw error.takeRetainedValue()
+			} else {
+				// FIXME: Throw a better error here
+				throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+			}
+		}
+
+		try Authorization.authorizationRightsUpdateDatabase(bundle: MainBundle, stringTableName: "Authorization")
 	}
 }
